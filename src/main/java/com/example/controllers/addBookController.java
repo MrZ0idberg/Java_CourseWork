5be0a5e1f43a8b78bba8ac6750c263b13a21f511
MrZoidberg <serega12034@gmail.com>
2022-04-30 20:30:30 +0300

    Написання функціоналу до контроллеру створення книжок

--- src/main/java/com/example/controllers/addBookController.java
+++ src/main/java/com/example/controllers/addBookController.java
@@ -4,8 +4,11 @@ import com.example.database.DatabaseHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,6 +32,9 @@ public class addBookController implements Initializable{
     @FXML
     private Button cancelButton;
 
+    @FXML
+    private AnchorPane rootPane;
+
     DatabaseHandler databaseHandler;
 
     @Override
@@ -36,14 +42,50 @@ public class addBookController implements Initializable{
         databaseHandler = new DatabaseHandler();
     }
 
+    /**
+     *Додавання книги в БД
+     */
     @FXML
     public void addBook(ActionEvent event){
+        String bookID = id.getText();
+        String bookAuthor = author.getText();
+        String bookName = name.getText();
+        String bookGenre = genre.getText();
+        String bookDepartment = department.getText();
+
+        boolean isEmptyTextField = bookID.isEmpty() || bookAuthor.isEmpty() || bookName.isEmpty()
+                || bookGenre.isEmpty() || bookDepartment.isEmpty();
+
+        if(!isEmptyTextField){
+            if(databaseHandler.addBook(bookAuthor, bookName, bookGenre, bookDepartment)){
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setHeaderText(null);
+                alert.setContentText("Книжку успішно додано до БД");
+                alert.showAndWait();
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setHeaderText(null);
+                alert.setContentText("Помилка при передачі запиту до БД");
+                alert.showAndWait();
+            }
+
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText(null);
+            alert.setContentText("Заповніть всі поля");
+            alert.showAndWait();
+        }
 
     }
 
+    /**
+     * Закриття вікна
+     */
     @FXML
     public void cancel(ActionEvent event){
-
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        stage.close();
     }
 
 
